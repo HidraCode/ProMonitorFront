3e400e06191401b81dd32d2e031301704b9f8805
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-08-30 13:57:01 -0300

    feat: criação da page EditalForm.jsx; Ajuste das rotas

--- Routes.jsx
+++ Routes.jsx
@@ -5,7 +5,7 @@ import Login from './src/pages/auth/login/Login.jsx';
 import RecuperarSenha from './src/pages/auth/login/RecuperarSenha.jsx';
 import VerificarCodigo from './src/pages/auth/login/VerificarCodigo.jsx';
 import LandingPage from './src/pages/LandingPage.jsx';
-// import EditalForm from './src/pages/professor/EditalForm.jsx';
+import EditalForm from './src/pages/professor/EditalForm.jsx';
 import AlterarDadosAluno from './src/pages/student/AlterarDadosAluno.jsx';
 
 function AppRoutes() {
@@ -18,7 +18,7 @@ function AppRoutes() {
         <Route path="/auth/password-recovery" element={<RecuperarSenha />} />
         <Route path="/auth/password-recovery/verify-code" element={<VerificarCodigo />} />
         <Route path="/student/update-data" element={<AlterarDadosAluno />} />
-        {/* <Route path="/professor/create-edital" element={<EditalForm />} /> */}
+        <Route path="/professor/create-edital" element={<EditalForm />} />
       </Routes>
     </Router>
   );
